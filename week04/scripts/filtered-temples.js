fa64a61b015b353eb938b097d6dba989580768c4
Deka-ops <gnwabuikwuchori@byupathway.edu>
2025-03-29 08:47:13 +0100

    update

---
+++ week04/scripts/filtered-temples.js
@@ -0,0 +1,68 @@
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+ 
+const today = new Date();
+currentyear.innerHTML = today.getFullYear()
+ 
+lastModified.innerHTML = document.lastModified
+ 
+
+const temples = [
+    {
+      templeName: "Aba Nigeria",
+      location: "Aba, Nigeria",
+      dedicated: "2005, August, 7",
+      area: 11500,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+      templeName: "Abidjan Ivory Coast Temple",
+      location: "Cocody, Abidjan, Cote d'ivoire",
+      dedicated: "2025, May, 01",
+      area: 74792,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+      templeName: "Apia Samoa Temple",
+      location: "Pesega Apia Samoa",
+      dedicated: "2005, September, 04",
+      area: 18691,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+      templeName: "Atlanta Georgia Temple",
+      location: "Georgia, United States",
+      dedicated: "2011, May, 1",
+      area: 34500,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+      templeName: "Brisbane Australia Temple",
+      location: "Queensland, Australia",
+      dedicated: "2003, June, 15",
+      area: 10700,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+      templeName: "Billing Temple",
+      location: "Montana, United States.",
+      dedicated: "1999, November, 20-21",
+      area: 33800,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+      templeName: "Mexico City Mexico",
+      location: "Mexico City, Mexico",
+      dedicated: "1983, December, 2",
+      area: 116642,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    // Add more temple objects here...
+  ];
