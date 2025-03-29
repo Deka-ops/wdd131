cd20aebdd6acf74b0bfb90df49691482c46f3782
Deka-ops <gnwabuikwuchori@byupathway.edu>
2025-03-29 10:16:01 +0100

    updated java

--- week04/scripts/filtered-temples.js
+++ week04/scripts/filtered-temples.js
@@ -22,7 +22,7 @@ const temples = [
       dedicated: "2025, May, 01",
       area: 74792,
       imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+      "https://lh3.googleusercontent.com/p/AF1QipOLwWAccWiDB9IboDA-hC3AUnjBgvCaNOVGLmA=s1360-w1360-h1020"
     },
     {
       templeName: "Apia Samoa Temple",
@@ -30,7 +30,7 @@ const temples = [
       dedicated: "2005, September, 04",
       area: 18691,
       imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/apia-samoa/400x250/apia-samoa-temple-lds-460475-wallpaper.jpg"
     },
     {
       templeName: "Atlanta Georgia Temple",
@@ -38,7 +38,7 @@ const temples = [
       dedicated: "2011, May, 1",
       area: 34500,
       imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/atlanta-georgia/400x250/atlanta-georgia-mormon-temple-900182-wallpaper.jpg"
     },
     {
       templeName: "Brisbane Australia Temple",
@@ -46,23 +46,41 @@ const temples = [
       dedicated: "2003, June, 15",
       area: 10700,
       imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/brisbane-australia/400x250/brisbane-australia-temple-lds-745088-wallpaper.jpg"
     },
     {
-      templeName: "Billing Temple",
+      templeName: "Billing Montana Temple",
       location: "Montana, United States.",
       dedicated: "1999, November, 20-21",
       area: 33800,
       imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/billings-montana/400x250/02-Billings-Montana-Temple-1572044.jpg"
     },
     {
-      templeName: "Mexico City Mexico",
-      location: "Mexico City, Mexico",
-      dedicated: "1983, December, 2",
-      area: 116642,
+      templeName: "Bern Switzerland Temple",
+      location: "Zollikofen, Switzerland",
+      dedicated: "1992, October, 23-25",
+      area: 35546,
       imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/bern-switzerland/400x250/bern-switzerland-temple-lds-1053202-wallpaper.jpg"
     },
     // Add more temple objects here...
+
+    {
+      templeName: "Hong-Kong China Temple",
+      location: "Kowloon City, Hong-Kong",
+      dedicated: "2022, June, 19",
+      area: 51921,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/hong-kong-china/400x250/hong_kong_china_temple_exterior1.jpeg"
+    },
+
+    {
+      templeName: "Switzerland Temple",
+      location: "Switzerland",
+      dedicated: "1992, October, 23-25",
+      area: 35546,
+      imageUrl:
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/bern-switzerland/400x250/bern-switzerland-temple-lds-935504-wallpaper.jpg"
+    },
   ];
