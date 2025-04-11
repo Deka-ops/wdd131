d4c67731cdc15ecbf290411a9a285cb6580f1899
Deka-ops <gnwabuikwuchori@byupathway.edu>
2025-04-11 05:41:44 +0100

    updated

--- scripts/ladybird.js
+++ scripts/ladybird.js
@@ -1,21 +1,21 @@
 
-  document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const menu = document.getElementById('menu');
 
     menuToggle.addEventListener('click', () => {
-      menu.classList.toggle('open');
+        menu.classList.toggle('open');
     });
-  });
+});
 
-  const currentyear = document.querySelector("#currentyear");
+const currentyear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
- 
+
 const today = new Date();
 currentyear.innerHTML = today.getFullYear()
- 
+
 lastModified.innerHTML = document.lastModified
 
 function shopNow() {
     window.location.href = "#shop";
-  }
+}
